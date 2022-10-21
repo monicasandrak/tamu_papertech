c38be38cbb3652b2a834ec92f6ffb4b41462b6e1
rissasriag <rissasriag@gmail.com>
2022-10-21 13:24:49 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,120 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+<!DOCTYPE html>
+<html lang="en">
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>Login</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="{{asset('template2')}}/css/font-face.css" rel="stylesheet" media="all">
+    <link href="{{asset('template2')}}/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="{{asset('template2')}}/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="{{asset('template2')}}vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="{{asset('template2')}}/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="{{asset('template2')}}/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="{{asset('template2')}}/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="{{asset('template2')}}/vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="{{asset('template2')}}/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="{{asset('template2')}}/vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="{{asset('template2')}}/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="{{asset('template2')}}/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="{{asset('template2')}}/css/theme.css" rel="stylesheet" media="all">
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <div class="page-content--bge5">
+            <div class="container">
+                <div class="login-wrap">
+                    <div class="login-content">
+                        <div class="login-logo">
+                            <a href="#">
+                                <img src="{{asset('template2')}}/images/icon/logo_papertech.png" alt="CoolAdmin">
+                            </a>
+                        </div>
+                        <div class="login-form">
+                            <form action="{{route('login')}}" method="post">
+                                @csrf
+                                <div class="form-group">
+                                    <label>Username</label>
+                                    <input class="au-input au-input--full" type="username" name="username" placeholder="Username">
+                                @error('username')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>  
+                                @enderror                   
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>  
+                                @enderror
+                                </div>
+                                <div class="login-checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{old ('remember') ? 'checked' : '' }}>Remember Me
+                                    </label>
+                                    <label>
+                                        @if (Route::has('password.request'))
+                                        <a href="{route ('password.request') }}">Forgotten Password?</a>
+                                        @endif
+                                    </label>
+                                </div>
+                                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">sign in</button>
+                               
+                            </form>
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
-        @endif
+        </div>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    </div>
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+    <!-- Jquery JS-->
+    <script src="{{asset('template2')}}/vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="{{asset('template2')}}/vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="{{asset('template2')}}/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="{{asset('template2')}}/vendor/slick/slick.min.js">
+    </script>
+    <script src="{{asset('template2')}}/vendor/wow/wow.min.js"></script>
+    <script src="{{asset('template2')}}/vendor/animsition/animsition.min.js"></script>
+    <script src="{{asset('template2')}}/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="{{asset('template2')}}/vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="{{asset('template2')}}/vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <script src="{{asset('template2')}}/vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="{{asset('template2')}}/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="{{asset('template2')}}/vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="{{asset('template2')}}/vendor/select2/select2.min.js">
+    </script>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+    <!-- Main JS-->
+    <script src="{{asset('template2')}}/js/main.js"></script>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+</body>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+</html>
+<!-- end document-->
